8e72c096fed60e6d05c514cba196135206e1fc6a
Reuel Freitas Cunha <54087058+reuel-freitas@users.noreply.github.com>
2022-01-02 18:48:09 -0300

    Post component testing

--- src/Components/PostCard/Post.js
+++ src/Components/PostCard/Post.js
@@ -1,9 +1,15 @@
 export const Post = ({ cover, title, body }) => {
     return (
-        <div style={{ backgroundColor: '#6e6e6e6e', padding: 10, borderRadius: 7 }}>
-            <img src={cover} alt={title} style={{ maxWidth: '100%', borderRadius: 7 }} />
-            <h3 style={{ color: '#eee', textAlign: 'left' }}>{title}</h3>
-            <p style={{ color: '#eee', textAlign: 'left' }}>{body}</p>
+        <div style={styles.postContainer}>
+            <img src={cover} alt={title} style={styles.postCover} />
+            <h3 style={styles.postText}>{title}</h3>
+            <p style={styles.postText}>{body}</p>
         </div>
     )
+}
+
+const styles = {
+    postContainer: { backgroundColor: '#6e6e6e6e', padding: 10, borderRadius: 7 },
+    postCover: { maxWidth: '100%', borderRadius: 7 },
+    postText: { color: '#eee', textAlign: 'left' }
 }
